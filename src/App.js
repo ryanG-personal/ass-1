40bff8dd61de5564ed616443fb87b8473e85494d
Gilday, Ryan <ryan.gilday@sky.uk>
2020-08-27 17:13:32 +0100

    Forget Css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import UserInput from './components/UserInput/UserInput'
 import UserOutput from './components/UserOutput/UserOutput'
 
 class App extends Component {
   state = {
-    username: 'newName'
+    username: 'Initial Name'
   }
 
   changeUsernameHandler= (event)=>{
@@ -15,11 +14,16 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <UserInput username ={this.state.username} changed={this.changeUsernameHandler} />
-        <UserOutput username = {this.state.username}/>
-        <UserOutput username = {this.state.username}/>
-        <UserOutput username = {this.state.username}/>
+      <div className="assignment1">
+        <UserInput
+         username ={this.state.username}
+         changed = {this.changeUsernameHandler} />
+        <UserOutput
+         username = {this.state.username}/>
+        <UserOutput
+         username = {this.state.username}/>
+        <UserOutput
+         username = {this.state.username}/>
       </div>
     );
   }
--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-const userInput = (props) =>{
-    return(
-    <input type="text" onChange={props.changed} value= {props.username}/>
-    )
-}
+const userInput = (props) =>  <input type="text" onChange={props.changed} value= {props.username}/>
 
 export default userInput
